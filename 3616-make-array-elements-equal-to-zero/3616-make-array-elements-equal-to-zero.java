9ee08241ad312f6147d64bb0a49e617397a25520
sidpanda29 <sidpandovski@gmail.com>
2025-10-28 22:12:23 -0500

    Time: 1 ms (82.63%), Space: 42 MB (92.22%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -1,7 +1,8 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-        if(nums.length == 1 && nums[0] == 0) return 2;
-        if(nums.length == 1 && nums[0] != 0) return 0;
+        
+        //if(nums.length == 1 && nums[0] == 0) return 2;
+        //if(nums.length == 1 && nums[0] != 0) return 0;
         int counter = 0;
         int curr = 0;
         for(int i = 0; i < nums.length; i++){
