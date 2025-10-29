4def829d3d59050f1c0b4185c52df30a6b68e29a
sidpanda29 <sidpandovski@gmail.com>
2025-10-29 16:30:47 -0500

    Time: 0 ms (100%), Space: 42.4 MB (76.82%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        // Can be done with recursion
+
+        // first check edge cases to start with
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+
+        // call the recursion
+        int temp = 0;
+        if(list1.val < list2.val){
+            temp = list1.val;
+            list1 = list1.next;
+            ListNode ret = new ListNode(temp, mergeTwoLists(list1, list2));
+            return ret;
+        }else{
+            temp = list2.val;
+            list2 = list2.next;
+            ListNode ret = new ListNode(temp, mergeTwoLists(list1, list2));
+            return ret;
+        }
+        
+    }
+
+    
+}
