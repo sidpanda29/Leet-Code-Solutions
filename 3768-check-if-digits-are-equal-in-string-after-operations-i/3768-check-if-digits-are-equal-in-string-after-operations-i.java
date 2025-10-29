a66268759811bb2886314f5de794e334f76d5960
sidpanda29 <sidpandovski@gmail.com>
2025-10-29 00:19:49 -0500

    Time: 6 ms (60.77%), Space: 44.7 MB (47.17%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,13 +1,21 @@
 class Solution {
     public boolean hasSameDigits(String s) {
+
+        // Create a recursive method
+
+        // If the length is three, the middle character doesn't matter, so if
+        // the ends are the same it returns true. This is the base case
         if(s.length() == 3) return s.charAt(0) == s.charAt(2);
 
+        // now turn the string into an int to mutate it, which is why we use a SB for
+        // temp instead of a regular string
         StringBuilder temp = new StringBuilder();
         for(int i = 0; i < s.length()-1; i++){
             int d = ((s.charAt(i) - '0') + (s.charAt(i + 1) - '0')) % 10;
             temp.append((char)('0' + d));
         }
 
+        // Call recursion
         return hasSameDigits(temp.toString());
     }
 }
