09b73ccb15d143f805d2696e578d43f5e59c67bd
sidpanda29 <sidpandovski@gmail.com>
2025-10-28 22:13:26 -0500

    Time: 1 ms (82.63%), Space: 42.2 MB (69.46%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -1,20 +1,16 @@
 class Solution {
     public int countValidSelections(int[] nums) {
         
-        //if(nums.length == 1 && nums[0] == 0) return 2;
-        //if(nums.length == 1 && nums[0] != 0) return 0;
         int counter = 0;
-        int curr = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 0){
-                //System.out.println("Made it");
-                //System.out.println(sumChecker(nums, i));
                 counter += sumChecker(nums, i);
             }
         }
         return counter;
     }
 
+    // helper method to check the sum before and after and return right amount
     private int sumChecker(int[] nums, int index){
         int prevSum = 0;
         int nextSum = 0;
@@ -22,13 +18,11 @@ class Solution {
         while(temp < index){
             prevSum += nums[temp];
             temp ++;
-            //System.out.println(prevSum);
         }
         temp = nums.length - 1;
         while(temp > index){
             nextSum += nums[temp];
             temp --;
-            //System.out.println(nextSum);
         }
         if(prevSum == nextSum) return 2;
         if(Math.abs(prevSum - nextSum) == 1) return 1;
