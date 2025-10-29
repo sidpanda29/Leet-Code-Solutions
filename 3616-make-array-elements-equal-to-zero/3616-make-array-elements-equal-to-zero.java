4369a9754f6000be27372deb3290e79544990616
sidpanda29 <sidpandovski@gmail.com>
2025-10-28 22:11:17 -0500

    Time: 1 ms (82.63%), Space: 42.4 MB (42.52%) - LeetHub

---
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int countValidSelections(int[] nums) {
+        if(nums.length == 1 && nums[0] == 0) return 2;
+        if(nums.length == 1 && nums[0] != 0) return 0;
+        int counter = 0;
+        int curr = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0){
+                //System.out.println("Made it");
+                //System.out.println(sumChecker(nums, i));
+                counter += sumChecker(nums, i);
+            }
+        }
+        return counter;
+    }
+
+    private int sumChecker(int[] nums, int index){
+        int prevSum = 0;
+        int nextSum = 0;
+        int temp = 0;
+        while(temp < index){
+            prevSum += nums[temp];
+            temp ++;
+            //System.out.println(prevSum);
+        }
+        temp = nums.length - 1;
+        while(temp > index){
+            nextSum += nums[temp];
+            temp --;
+            //System.out.println(nextSum);
+        }
+        if(prevSum == nextSum) return 2;
+        if(Math.abs(prevSum - nextSum) == 1) return 1;
+        return 0;
+    }
+}
