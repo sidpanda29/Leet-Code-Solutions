9006d9f0ae514d832d30bb65d3d7e8808b4db797
sidpanda29 <sidpandovski@gmail.com>
2025-10-31 00:35:17 -0500

    Time: 1 ms (100%), Space: 45.7 MB (7.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,26 +11,25 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
+        // setting up the temp ListNodes to work with
         ListNode tem = new ListNode(1);
         ListNode ret = new ListNode(0, tem);
         // setting up the base case
         if(l1.next == null && l2.next == null){
-            System.out.println("ret1");
             ret.val = (l1.val+l2.val) % 10;
             if((l1.val + l2.val) < 10) ret.next = null;
             return ret;
+        // setting up the cases where if one is null
         }else if(l1.next == null){
-            System.out.println("ret2");
             if((l1.val + l2.val) < 10) tem.val = 0;
             ListNode ret2 = new ListNode((l1.val + l2.val) % 10, addTwoNumbers(tem, l2.next));
             return ret2;
         }else if(l2.next == null){
-            System.out.println("ret3");
             if((l1.val + l2.val) < 10) tem.val = 0;
             ListNode ret3 = new ListNode((l1.val + l2.val) % 10, addTwoNumbers(l1.next, tem));
             return ret3;
+        // setting up the case where they both have another value after
         }else{
-            System.out.println("ret4");
             if((l1.val + l2.val) >= 10) l1.next.val ++;
             ListNode ret4 = new ListNode((l1.val + l2.val) % 10, addTwoNumbers(l1.next, l2.next));
             return ret4;
