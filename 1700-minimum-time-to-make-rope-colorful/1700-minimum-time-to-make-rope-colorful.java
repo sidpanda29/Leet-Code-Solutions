242fc516a332ece9b70ac76dd8f10e3d7c3c5202
sidpanda29 <sidpandovski@gmail.com>
2025-11-02 19:17:53 -0600

    Time: 1188 ms (5.24%), Space: 91.9 MB (5.68%) - LeetHub

---
+++ 1700-minimum-time-to-make-rope-colorful/1700-minimum-time-to-make-rope-colorful.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+
+        // if the string is 0 or 1, there is no duplicate balloons, return original
+        if(colors.length() <= 1) return 0;
+
+        // Counter, compare start to final. Put the result into counter to be returned
+        int counter = 0;
+        int startIndex = 0;
+        int finalIndex = 0;
+        int max = 0;
+        boolean maxTriggered = false;
+        for(int i = 0; i < colors.length(); i ++){
+            startIndex = i;
+            finalIndex = i+1;
+            while(finalIndex < colors.length() && colors.charAt(finalIndex) == colors.charAt(startIndex)){
+                finalIndex ++;
+            }
+            System.out.print(startIndex + ", " + finalIndex);
+            System.out.println();
+            
+            max = neededTime[startIndex];
+            for(int j = startIndex; j < finalIndex; j++){
+                if(max < neededTime[j]) max = neededTime[j];
+            }
+            for(int j = startIndex; j < finalIndex; j++){
+                if(max == neededTime[j] && maxTriggered == false) maxTriggered = true;
+                else if(max != neededTime[j] || maxTriggered == true) counter += neededTime[j];
+            }
+            i = finalIndex - 1;
+            maxTriggered = false;
+        }
+
+        return counter;
+
+        /*
+        Check the first set of Indexes, the first index will start at 0 then have a run.
+        the next will start one after the last index ended.*/
+    }
+}
