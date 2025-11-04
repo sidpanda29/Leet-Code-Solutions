23a6c486be4e5dea152c64a30e1d3bad652f1a80
sidpanda29 <sidpandovski@gmail.com>
2025-11-03 22:06:54 -0600

    Time: 26 ms (42.28%), Space: 147.2 MB (55.1%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.java
@@ -1,57 +1,65 @@
 class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
         
+        // creating a board for the problem
         int[][] board = new int[m][n];
         
+        // adding the walls to the board to start
         for(int i = 0; i < walls.length; i ++){
             board[walls[i][0]][walls[i][1]] = 1;
         }
         
-        int guardsX = 0;
+        // initializing the index in which the guards will go
         int guardsY = 0;
+        int guardsX = 0;
         boolean breakCond = false;
-        for(int j = 0; j < guards.length; j ++){
-            guardsX = guards[j][0];
-            guardsY = guards[j][1];
-            if(board[guardsX][guardsY] == -1 && (m == 1 || n == 1)) continue;
-            board[guardsX][guardsY] = -1;
 
-            
-            while(guardsY < n-1 && board[guardsX][guardsY + 1] != 1){
-                guardsY ++;
-                board[guardsX][guardsY] = -1;
-                if(guardsY + 1 < n-1 && board[guardsX][guardsY + 1] == 1){
-                    guardsY = guards[j][1];
+        // adding guard to the board
+        for(int j = 0; j < guards.length; j ++){
+            guardsY = guards[j][0];
+            guardsX = guards[j][1];
+            if(board[guardsY][guardsX] == -1 && (m == 1 || n == 1)) continue;
+            board[guardsY][guardsX] = -1;
+
+            // adding guards in each direction on the X axis to begin with
+            while(guardsX < n-1 && board[guardsY][guardsX + 1] != 1){
+                guardsX ++;
+                board[guardsY][guardsX] = -1;
+                if(guardsX + 1 < n-1 && board[guardsY][guardsX + 1] == 1){
+                    guardsX = guards[j][1];
                     break;
                 }
-            }while(guardsY > 0 && board[guardsX][guardsY - 1] != 1){
-                guardsY --;
-                board[guardsX][guardsY] = -1;
-                if(guardsY - 1 > 0 && board[guardsX][guardsY - 1] == 1){
-                    guardsY = guards[j][1];
+            }while(guardsX > 0 && board[guardsY][guardsX - 1] != 1){
+                guardsX --;
+                board[guardsY][guardsX] = -1;
+                if(guardsX - 1 > 0 && board[guardsY][guardsX - 1] == 1){
+                    guardsX = guards[j][1];
                     break;
                 }
             }
             
-            guardsX = guards[j][0];
-            guardsY = guards[j][1];
+            // resetting to the initial conditions then doing the same in the Y direction
+            guardsY = guards[j][0];
+            guardsX = guards[j][1];
 
-            while(guardsX < m-1 && board[guardsX + 1][guardsY] != 1){
-                guardsX ++;
-                board[guardsX][guardsY] = -1;
-                if(guardsX + 1 < m-1 && board[guardsX + 1][guardsY] == 1){
-                    guardsX = guards[j][0];
+            while(guardsY < m-1 && board[guardsY + 1][guardsX] != 1){
+                guardsY ++;
+                board[guardsY][guardsX] = -1;
+                if(guardsY + 1 < m-1 && board[guardsY + 1][guardsX] == 1){
+                    guardsY = guards[j][0];
                     break;
                 }
-            }while(guardsX > 0 && board[guardsX -1][guardsY] != 1){
-                guardsX --;
-                board[guardsX][guardsY] = -1;
-                if(guardsX -1 > 0 && board[guardsX - 1][guardsY] == 1){
-                    guardsX = guards[j][0];
+            }while(guardsY > 0 && board[guardsY -1][guardsX] != 1){
+                guardsY --;
+                board[guardsY][guardsX] = -1;
+                if(guardsY -1 > 0 && board[guardsY - 1][guardsX] == 1){
+                    guardsY = guards[j][0];
                     break;
                 }
             }
         }
+        
+        //iterating through and adding one to the counter whenever we see a guard position (-1)
         int counter = 0;
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[i].length; j++){
@@ -61,16 +69,4 @@ class Solution {
         return counter;
 
     }
-
-    /*public int printBoard(int[][] board){
-        int counter = 0;
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                //System.out.print(board[i][j] + ", ");
-                if(board[i][j] == 0) counter++;
-            }
-            //System.out.println();
-        }
-        return counter;
-    }*/
 }
