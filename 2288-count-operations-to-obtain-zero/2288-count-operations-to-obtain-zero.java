79973ae9e6dca840cd27695f8f3e613e20cd685a
sidpanda29 <sidpandovski@gmail.com>
2025-11-12 09:28:49 -0600

    Time: 1 ms (61.62%), Space: 42.1 MB (75.55%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -1,12 +1,18 @@
 class Solution {
     public int countOperations(int num1, int num2) {
+        // check base case
         if(num1 == 0 || num2 == 0) return 0;
-        int counter = 0;
+        
+        // counter to return
+        int counter = 1;
+
+        // conditionals
         while(num1 != num2 && num1 > 0 && num2 > 0){
             if(num1 > num2) num1 = num1 - num2;
             else num2 = num2 - num1;
             counter ++;
         }
-        return counter + 1;
+        
+        return counter;
     }
 }
